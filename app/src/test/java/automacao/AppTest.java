6e30d3c03d94ebfa4bb722a2cad375c92c1a107d
Beatriz Silva Tavares Santiago <beatriz.santiago@deal.br>
2021-07-21 11:27:36 -0300

    adiciona teste de criação de usuario com sucesso

--- app/src/test/java/automacao/AppTest.java
+++ app/src/test/java/automacao/AppTest.java
@@ -1,24 +1,37 @@
 
 package automacao;
 
+import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.basePath;
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 public class AppTest {
     @Test
-    public void testeVideoAula() {
+    public void testeListaMetadosUsuario() {
         when().
                 get("https://reqres.in/api/users?page=2").
-        then().
+                then().
                 statusCode(HttpStatus.SC_OK).
                 body("page", is(2)).
                 body("data", is(notNullValue()));
-
     }
+    @Test
+    public void testeCriarUsuarioComSucesso() {
+        given().log().all().
+                contentType(ContentType.JSON).
+                body("{\"name\": \"rafael\", \"job\": \"eng test\"}").
+        when().
+                post("https://reqres.in/api/users").
+        then().
+                statusCode(HttpStatus.SC_CREATED).
+                body("name", is("rafael"));
+
+        }
+
+
 }
