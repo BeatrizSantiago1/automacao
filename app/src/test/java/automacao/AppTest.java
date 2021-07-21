35cab94f291ba6ae853af4e68d05b5305fa27554
Beatriz Silva Tavares Santiago <beatriz.santiago@deal.br>
2021-07-21 10:19:03 -0300

    insere dependencia rest assured e criar teste de listar metadata

--- app/src/test/java/automacao/AppTest.java
+++ app/src/test/java/automacao/AppTest.java
@@ -1,11 +1,13 @@
 
 package automacao;
 
+import org.apache.http.HttpStatus;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.basePath;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 public class AppTest {
@@ -14,9 +16,9 @@ public class AppTest {
         when().
                 get("https://reqres.in/api/users?page=2").
         then().
-                statusCode(200);
-
-
+                statusCode(HttpStatus.SC_OK).
+                body("page", is(2)).
+                body("data", is(notNullValue()));
 
     }
 }
